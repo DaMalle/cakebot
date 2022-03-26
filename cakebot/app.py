13eac2a1d72ae67c50339d4678f0ecaf537f5e05
Marcus <58878411+DaMalle@users.noreply.github.com>
2022-03-26 19:33:35 +0100

    refactored app.py

--- cakebot/app.py
+++ cakebot/app.py
@@ -1,20 +1,17 @@
-import youtube_dl
 import discord
 from discord.ext import commands
 
 import os
 from dotenv import load_dotenv
 
-def main():
+def main() -> None:
     load_dotenv() # Get access to .env
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
     
-    #intents = discord.Intents().all()
-    #client = discord.Client(intents=intents)
-    bot = commands.Bot(command_prefix='!') #, intents=intents)
-
-    bot.load_extension('commands.general')
-    bot.load_extension('commands.music')
+    bot = commands.Bot(command_prefix='!')
+    
+    extensions = ['commands.general', 'commands.music']
+    for extension in extensions: bot.load_extension(extension)
     bot.run(DISCORD_TOKEN)
 
 if __name__ == '__main__':
